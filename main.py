403415b0e1d6d96b143c6e17daa284f2312365d3
Marco Capotondi <marcocapotondi@fastwebnet.it>
2017-03-05 18:57:42 +0100

    Main modified

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from gym.spaces.box import Box
 from DQN import experience_replay
 from NET import *
 
+'''
 def processState(state):
 	return np.reshape(states,[21168])
 
@@ -20,103 +21,123 @@ def updateTargetGraph(tfVars,tau):
 def updateTarget(op_holder,sess):
     for op in op_holder:
         sess.run(op)
+'''
 
-#import gym
-#env = gym.make('Copy-v0')
-#env.reset()
-#env.render()
 
-batch_size = 32 #How many experiences to use for each training step.
-update_freq = 4 #How often to perform a training step.
+env = gym.make('Breakout-v0')
+
+
+batch_size = 10 #How many experiences to use for each training step.
+update_freq = 1000 #How often to perform a training step.
 y = .99 #Discount factor on the target Q-values
 startE = 1 #Starting chance of random action
 endE = 0.1 #Final chance of random action
 anneling_steps = 10000. #How many steps of training to reduce startE to endE.
-num_episodes = 10000 #How many episodes of game environment to train network with.
-pre_train_steps = 10000 #How many steps of random actions before training begins.
-max_epLength = 50 #The max allowed length of our episode.
-path = "./dqn" #The path to save our model to.
-h_size = 512 #The size of the final convolutional layer before splitting it into Advantage and Value streams.
-tau = 0.001 #Rate to update target network toward primary network
+num_episodes = 100 #How many episodes of game environment to train network with.
+pre_train_steps = 1000 #How many steps of random actions before training begins.
+max_epLength = 1000 #The max allowed length of our episode.
+#path = "./dqn" #The path to save our model to.
+tau = 0.1 #Rate to update target network toward primary network
 
 
 total_steps = 0
+#totale
 myBuffer = experience_replay()
 
-image = (imread("atari.png")[:,:,:3]).astype(float32)
-image = image - mean(image)
-xdim = image.shape[0:]
-x = tf.placeholder(tf.float32, (None,) + xdim)
-weights = load("weights_alexnet.py").item()
 
-
-e = startE
+epsilon = startE
 stepDrop = (startE - endE)/anneling_steps
-#Costructors of the Nets
 
-targetQN = Q_Net()
-mainQN = Q_Net()
-targetQN.Preprocessing(weights,x)
-#targetQN.Train()
-targetQN.Predict()
-mainQN.Preprocessing(weights,x)
-mainQN.Predict()
-mainQN.Train(targetQN)
+
+QTarget = Q_Net()
+QMain = Q_Net()
 
 init = tf.initialize_all_variables()
+train = tf.trainable_variables()
+#saver = tf.train.Saver()
+sess = tf.Session()
 
 
-#targetops
-trainables = tf.trainable_variables()
-targetOps = updateTargetGraph(trainables,tau)
-
-with tf.Session() as sess:
-    sess.run(init)
-    #
-    for i in range(num_episodes):
-            episodeBuffer = experience_replay()
-            #Reset environment and get first new observation
-            s = env.reset()
-            s = processState(s)
-            d = False
-            rAll = 0
-            j = 0
-            #The Q-Network
-            while j < max_epLength: #If the agent takes longer than 200 moves to reach either of the blocks, end the trial.
-                j+=1
-                #Choose an action by greedily (with e chance of random action) from the Q-network
-                if np.random.rand(1) < e or total_steps < pre_train_steps:
-                    a = np.random.randint(0,2)
-                else:
-                    a = sess.run(mainQN.argmax,feed_dict={mainQN.scalarInput:[s]})[0]
-                s1,r,d = env.step(a)
-                s1 = processState(s1)
-                total_steps += 1
-                episodeBuffer.add(np.reshape(np.array([s,a,r,s1,d]),[1,5])) #Save the experience to our episode buffer.
-                
-                if total_steps > pre_train_steps:
-                    if e > endE:
-                        e -= stepDrop
-                    
-                    if total_steps % (update_freq) == 0:
-                        trainBatch = myBuffer.sample(batch_size) #Get a random batch of experiences.
-                        #Below we perform the Double-DQN update to the target Q-values
-                        Q1 = sess.run(mainQN.argmax,feed_dict={mainQN.scalarInput:np.vstack(trainBatch[:,3])})
-                        Q2 = sess.run(targetQN.prob,feed_dict={targetQN.scalarInput:np.vstack(trainBatch[:,3])})
-                        end_multiplier = -(trainBatch[:,4] - 1)
-                        targetQ = trainBatch[:,2] + (y*Q2 * end_multiplier)
-                        #Update the network with our target values.
-                        _ = sess.run(mainQN.updateWeights, \
-                            feed_dict={mainQN.scalarInput:np.vstack(trainBatch[:,0]),mainQN.targetQ:targetQ, mainQN.actions:trainBatch[:,1]})
-                        
-                        updateTarget(targetOps,sess) #Set the target network to be equal to the primary network.
-                rAll += r
-                s = s1
-                
-                if d == True:
-
-                    break
+total_steps = 0
+reward = []
+total_reward = 0.
+k=0
+sess.run(init)
+prob = 0
+for i in range(num_episodes):
+		#singolo episodio
+        episodeBuffer = experience_replay()
+        #Reset environment and get first new observation
+        state = env.reset()
+        #env.render()
+        #s = processState(s)#di quanto??
+        done = False
+        reward_tot = 0
+        j = 0
+        #The Q-Network
+        while j < max_epLength: #If the agent takes longer than 200 moves to reach either of the blocks, end the trial.
             
-            myBuffer.add(episodeBuffer.buffer)
+            j+=1
+            #print j
+            
+            #Choose an action by greedily (with e chance of random action) from the Q-network
+            if (np.random.rand(1) < epsilon) or (total_steps < pre_train_steps):
+                action = env.action_space.sample()
+            else:
+                action = sess.run(QMain.argmax,feed_dict={QMain.input:[state]})
+            #env.render()
+            newstate,reward,done,info = env.step(action)
+            #newstate = processState(s1)
+            #print action,reward,done
+            total_steps += 1
+
+            if(reward != 0): 
+                k += 1 
+                print(reward,k,total_steps)
+
+            episodeBuffer.add(np.reshape(np.array([state,action,reward,newstate,done]),[1,5])) #Save the experience to our episode buffer.
+            
+            if total_steps > pre_train_steps:
+                if epsilon > endE:
+                    epsilon -= stepDrop
+                
+                if total_steps % (update_freq) == 0:
+
+                    trainBatch = myBuffer.sample(batch_size) #Get a random batch of experiences.
+                    #Below we perform the Double-DQN update to the target Q-values
+                    #Q1 = sess.run(QMain.prob,feed_dict={QMain.input:trainBatch[:,3]})
+
+                    Q2 = np.zeros((batch_size,6))
+                    Q1 = np.zeros((batch_size,6))
+                    QValue = np.zeros(batch_size)
+                    MaxQ2 = np.zeros(batch_size)
+                    targetQ = np.zeros(batch_size)
+
+                    end_multiplier = -(trainBatch[:,4] - 1)
+
+                    for l in range(batch_size):
+                        #print(l,total_steps)
+                        print(trainBatch[:,1])
+                        Q2[l] = sess.run(QTarget.prob,feed_dict={QTarget.input:[trainBatch[:,3][l]]})
+                        MaxQ2[l] = np.amax(Q2[l])
+                        print("uajo")
+                        targetQ[l] = trainBatch[l,2] + (y*MaxQ2[l]*end_multiplier[l])
+                        #print(trainBatch[:,0][l].shape,targetQ[l].shape,trainBatch[:,1][l].shape)
+                        _= sess.run(QMain.updateWeights,feed_dict={QMain.input:[trainBatch[:,0][l]],QMain.target:[targetQ[l]], QMain.actions:[trainBatch[:,1][l]]})
+                        #print(prob)
+
+                    #Update the network with our target values.
+                    
+                    
+                    
+                    #updateTarget(targetOps,sess) #Set the target network to be equal to the primary network.
+            total_reward += reward
+            state = newstate
+            
+            if done == True:
+
+                break
+        #
+        myBuffer.add(episodeBuffer.buffer)
 
         
